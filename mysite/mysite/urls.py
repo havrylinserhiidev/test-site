6ba0291cb5c7aeb32c000463f279a07178634b8f
hs <sgavrilyn@gmail.com>
2022-12-27 01:50:11 +0200

    Initial commit

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from news.views import index
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('news/', include('news.urls')),
+]
